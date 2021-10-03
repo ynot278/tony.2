a1c420088990b2ef31b67722e68cc5bd17ec6c2e
ptoughneigh <58543031+ynot278@users.noreply.github.com>
2021-10-03 01:55:16 +0000

    Forgot to version control:
    Day 1: Set up runsim main function to read command line arguments, validate arguments. Made makefile as well.
    
    Day 2: Set up shared memory and created testing.data along with testsim.
    
    Day 3: Attached pointer to shared memory and WIP: fork.

--- main.c
+++ main.c
@@ -102,25 +102,7 @@ int main(int argc, char *argv[]) {
 	} 
 	if (cpid == 0){
 		printf("Child PID is %ld\n", (long)getpid());
-	} else {
-			do {
-				w = waitpid(cpid, &status, WUNTRACED | WCONTINUED);
-				if (w == -1){
-					perror("Waitpid");
-					exit(EXIT_FAILURE);
-				}
-				if (WIFEXITED(status)){
-					printf("exited, status = %d\n", WEXITSTATUS(status));
-				} else if (WIFSIGNALED(status)) {
-					printf("killed by signal %d\n", WTERMSIG(status));
-				} else if (WIFSTOPPED(status)){
-					printf("stopped by signal %d\n", WSTOPSIG(status));
-				} else if (WIFCONTINUED(status)){
-					printf("continued\n");
-				}
-			} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-			exit(EXIT_SUCCESS);
-	}
+	} 
 
 	//detach
 	if (shmdt(shmp) ==  -1){
